932a4575d436318cb2824d190509c82a7cb19fd6
Florian Schuler <Florian Schuler>
2016-06-18 15:36:48 +0200

    cartrige type exposes rom itself and type now

--- src/gb/catridge.rs
+++ src/gb/catridge.rs
@@ -1,33 +1,50 @@
 use std::path::Path;
-use std::fmt::Debug;
+use std::result::Result;
+use std::fs::File;
+use std::io::Error;
+use std::io::Read;
+
 
 #[derive(Debug)]
 pub struct Cartrige {
-    pub Type : CartridgeType
-
+    pub cartirge_type: CartridgeType,
+    pub rom: Vec<u8>,
 }
 
 impl Cartrige {
-    pub fn from_path(p : &Path) -> Cartrige {
-        Cartrige { 
-            Type : CartridgeType::from_u8(0)
-        }
+    pub fn from_path(path: &Path) -> Result<Cartrige, Error> {
+        let mut file = try!(File::open(path));
+        let mut buff: Vec<u8> = Vec::new();
+        try!(file.read_to_end(&mut buff));
+        let c = Cartrige {
+            cartirge_type: CartridgeType::from_u8(buff[0x0147]),
+            rom: buff,
+        };
+        Ok(c)
     }
 }
 
 #[derive(Debug)]
+#[derive(PartialEq)]
 pub enum CartridgeType {
     Plain,
-    MBC1,
-    Unknown
+    Mbc1,
+    Unknown,
 }
 
 impl CartridgeType {
-    pub fn from_u8(value : u8) -> CartridgeType {
+    pub fn from_u8(value: u8) -> CartridgeType {
         match value {
             0 => CartridgeType::Plain,
-            1 => CartridgeType::MBC1,
-            _ => CartridgeType::Unknown 
+            1 => CartridgeType::Mbc1,
+            _ => CartridgeType::Unknown, 
         }
     }
+}
+
+
+#[test]
+fn cartrige_from_u8() {
+    let ct = CartridgeType::from_u8(1);
+    assert!(ct == CartridgeType::Mbc1);
 }
