bf226ac7801aabe808441a346791d79df5af4776
theonly112 <theonly112@gmail.com>
2017-01-22 15:52:55 +0100

    reuse texture and clean up display implementation

--- src/gb/display.rs
+++ src/gb/display.rs
@@ -1,6 +1,7 @@
 use gb::gpu::Color;
 
 use sdl2::render::Renderer;
+use sdl2::render::Texture;
 use sdl2::pixels::PixelFormatEnum;
 #[allow(unused_imports)]
 use sdl2::event::Event;
@@ -18,6 +19,7 @@ pub trait Display {
 
 pub struct SdlDisplay<'window> {
     pub renderer: Renderer<'window>,
+    texture: Texture,
     #[allow(dead_code)]
     context: Sdl,
     last_frame_time: f64,
@@ -31,16 +33,20 @@ impl<'window> SdlDisplay<'window> {
 
         let window = video_subsystem.window("rsgb", 160, 144)
             .position_centered()
-            .opengl()
+            .build()
+            .unwrap();
+        let renderer = window.renderer()
+            .accelerated()
             .build()
             .unwrap();
 
+        let texture = renderer.create_texture_streaming(PixelFormatEnum::RGB24, 160, 144)
+            .unwrap();
+
         SdlDisplay {
-            renderer: window.renderer()
-                .accelerated()
-                .build()
-                .unwrap(),
+            renderer: renderer,
             context: context,
+            texture: texture,
             last_frame_time: 0f64,
         }
     }
@@ -49,21 +55,10 @@ impl<'window> SdlDisplay<'window> {
 
 impl<'window> Display for SdlDisplay<'window> {
     fn draw(&mut self, framebuffer: [Color; 160 * 144]) {
-        if self.last_frame_time == 0f64 {
-            self.last_frame_time = time::precise_time_s();
-        } else {
-            let current = time::precise_time_s();
-            let delta = current - self.last_frame_time;
-            self.last_frame_time = current;
-            println!("FPS {}", 1f64 / delta);
-        }
+        self.print_debug_info();
 
-
-        let mut texture = self.renderer
-            .create_texture_streaming(PixelFormatEnum::RGB24, 160, 144)
-            .unwrap();
-
-        texture.with_lock(None, |buffer: &mut [u8], pitch: usize| {
+        self.texture
+            .with_lock(None, |buffer: &mut [u8], pitch: usize| {
                 for y in 0..144 {
                     for x in 0..160 {
                         let color = framebuffer[y * 160 + x];
@@ -78,20 +73,35 @@ impl<'window> Display for SdlDisplay<'window> {
 
 
         self.renderer.clear();
-        self.renderer.copy(&texture, None, None).unwrap();
+        self.renderer.copy(&self.texture, None, None).unwrap();
         self.renderer.present();
-
-        // let mut event_pump = self.context.event_pump().unwrap();
-        //
-        // 'running: loop {
-        // for event in event_pump.poll_iter() {
-        // match event {
-        // Event::Quit { .. } |
-        // Event::KeyDown { keycode: Some(Keycode::Escape), .. } => break 'running,
-        // _ => {}
-        // }
-        // }
-        // The rest of the game loop goes here...
-        // }
     }
 }
+
+impl<'window> SdlDisplay<'window> {
+    fn print_debug_info(&mut self) {
+        if self.last_frame_time == 0f64 {
+            self.last_frame_time = time::precise_time_s();
+        } else {
+            let current = time::precise_time_s();
+            let delta = current - self.last_frame_time;
+            self.last_frame_time = current;
+            println!("Frametime {}s", delta);
+        }
+
+    }
+}
+
+
+// let mut event_pump = self.context.event_pump().unwrap();
+//
+// 'running: loop {
+// for event in event_pump.poll_iter() {
+// match event {
+// Event::Quit { .. } |
+// Event::KeyDown { keycode: Some(Keycode::Escape), .. } => break 'running,
+// _ => {}
+// }
+// }
+// The rest of the game loop goes here...
+// }
